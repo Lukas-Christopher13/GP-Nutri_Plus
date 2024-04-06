6f9ad10ec16ab25af44d553559cb382e38b0468f
Arthur Pereira da Silva <arthurps884@gmail.com>
2024-04-06 13:45:50 -0300

    desenvolvimento inicial

---
+++ app/forms/forms.py
@@ -0,0 +1,15 @@
+# forms/forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField, FloatField
+from wtforms.validators import DataRequired
+
+class DietForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    objective = TextAreaField('Objective')
+    restrictions = TextAreaField('Restrictions')
+    duration = IntegerField('Duration (days)', validators=[DataRequired()])
+
+class FoodForm(FlaskForm):
+    name = StringField('Food Name', validators=[DataRequired()])
+    quantity = FloatField('Quantity (grams)', validators=[DataRequired()])
--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-from app import create_app
+# app.py
 
-app = create_app("develop")
+from flask import Flask
+from app.ext.db import db
+from app.models import *
+from app.controllers import *
+from app.controllers.diet.diet_controller import diet_bp
 
-if __name__ == "__main__":
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SECRET_KEY'] = 'mysecretkey'
+db.init_app(app)
+
+# Registrar blueprints
+app.register_blueprint(diet_bp)
+
+if __name__ == '__main__':
     app.run(debug=True)
