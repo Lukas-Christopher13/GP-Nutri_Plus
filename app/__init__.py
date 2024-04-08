dca692fe1550861a98d772997a920f19ececa04d
Arthur Pereira da Silva <arthurps884@gmail.com>
2024-04-08 11:26:49 -0300

    Correção de erros nos arquivos init e rum
    Correção de erro que ocasionou a exceção TemplateNotFound

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from config import config
 from .ext.db import db
 from .ext.bootstrap5 import bootstrap
 
+from app.controllers.diet.diet_controller import diet_bp
+
+
 
 app = Flask(__name__)
 
@@ -18,7 +21,7 @@ def create_app(config_name: str):
     app.register_blueprint(home_bluesprint)
 
     from .controllers.diet import diet as diet_bluesprint
-    app.register_blueprint(diet_bluesprint)
+    app.register_blueprint(diet_bp)
 
     return app
 
--- run.py
+++ run.py
@@ -1,16 +1,13 @@
 
-from flask import Flask
+from app import create_app
 from app.ext.db import db
 from app.models import *
 from app.controllers import *
-from app.controllers.diet.diet_controller import diet_bp
 
-app = Flask(__name__)
+app = create_app("develop")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.config['SECRET_KEY'] = 'mysecretkey'
-db.init_app(app)
 
-app.register_blueprint(diet_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
