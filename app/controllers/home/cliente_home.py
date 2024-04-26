910e3d3c16c2c8e77f1cb2a9199669ae1973522c
Lukas-Christipher13 <lukas.santana@aluno.uepb.edu.br>
2024-04-26 09:34:37 -0300

    adicionando style

--- app/controllers/home/cliente_home.py
+++ app/controllers/home/cliente_home.py
@@ -2,9 +2,10 @@ from . import home
 
 from flask import render_template
 
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 @home.route("/cliente_home_page")
 @login_required
 def cliente_home_page():
-    return render_template("home/cliente_home.html")
+    name = current_user.full_name
+    return render_template("home/cliente_home.html", name=name)
