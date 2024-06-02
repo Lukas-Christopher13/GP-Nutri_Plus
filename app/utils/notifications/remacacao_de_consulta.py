02bfe6e962cef4fc026314df48f414aa3eda38f8
Lukas-Christopher13 <lukas.santana@aluno.uepb.edu.br>
2024-06-02 11:19:26 -0300

    logs adicionados as notificações

--- app/utils/notifications/remacacao_de_consulta.py
+++ app/utils/notifications/remacacao_de_consulta.py
@@ -5,6 +5,7 @@ from ...ext.db import db
 from ...ext.mail import mail
 from ...models.cliente_model import Cliente
 from ...models.nuticionista_model import Notification
+from ...utils.log import my_log
 
 def notificar(cliente: Cliente, message):
     nutricionista = cliente.nutricionista_id
@@ -14,9 +15,13 @@ def notificar(cliente: Cliente, message):
     db.session.add(notification)
     db.session.commit()
 
+    my_log.info("Notificação Enviada!")
+
     if verify_if_notification_is_sendend(notification):
         db.session.add(notification)
         db.session.commit()
+
+        my_log.info("A notificação teve que ser reenviada!")
         
 def verify_if_notification_is_sendend(notification: Notification):
     notification = Notification.query.filter_by(id = notification.id).first()
