4177710e46ddc4cca62860ecac5492ddfac64685
Lukas-Christopher13 <lukas.santana@aluno.uepb.edu.br>
2024-06-02 11:16:42 -0300

    adicionado verificação extra para o envio de notificação

--- app/utils/notifications/remacacao_de_consulta.py
+++ app/utils/notifications/remacacao_de_consulta.py
@@ -6,11 +6,21 @@ from ...ext.mail import mail
 from ...models.cliente_model import Cliente
 from ...models.nuticionista_model import Notification
 
-def notificar_remaracacao_de_consulta(cliente: Cliente):
-    message = f"O cliente {cliente.full_name} deseja remarcar a data de sua consulta!"
+def notificar(cliente: Cliente, message):
     nutricionista = cliente.nutricionista_id
  
     notification = Notification(message=message, nutricionista_id=nutricionista)
 
     db.session.add(notification)
     db.session.commit()
+
+    if verify_if_notification_is_sendend(notification):
+        db.session.add(notification)
+        db.session.commit()
+        
+def verify_if_notification_is_sendend(notification: Notification):
+    notification = Notification.query.filter_by(id = notification.id).first()
+
+    if notification is None:
+        return True
+    return False
