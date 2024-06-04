a9e874c78f21cc6435a4d122b6060da217ede16b
Lukas-Christopher13 <lukas.santana@aluno.uepb.edu.br>
2024-06-04 15:01:01 -0300

    historico de consultas adicionado

--- app/controllers/cliente/historioco.py
+++ app/controllers/cliente/historioco.py
@@ -2,10 +2,15 @@ from . import cliente
 
 from flask import render_template
 
+from flask_login import login_required, current_user
+
+from ...models.consulta_model import Consulta
+
+
 @cliente.route("/historico")
+@login_required
 def historico():
-    
 
+    consultas = Consulta.query.filter_by(cliente_id = current_user.id).all()
 
-
-    return render_template("cliente/historico.html")
+    return render_template("cliente/historico.html", consultas=consultas)
