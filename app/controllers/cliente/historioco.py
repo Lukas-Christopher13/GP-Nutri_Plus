90a6caf3ee8b5c232d3b352c4ac9e5213d70da23
Lukas-Christopher13 <lukas.santana@aluno.uepb.edu.br>
2024-06-05 17:42:29 -0300

    terminar dps

--- app/controllers/cliente/historioco.py
+++ app/controllers/cliente/historioco.py
@@ -4,6 +4,7 @@ from flask import render_template
 
 from flask_login import login_required, current_user
 
+from ...models.models import Diet
 from ...models.consulta_model import Consulta
 
 
@@ -11,6 +12,7 @@ from ...models.consulta_model import Consulta
 @login_required
 def historico():
 
+    dietas = Diet.query.filter_by(cliente_id = current_user.id).all()
     consultas = Consulta.query.filter_by(cliente_id = current_user.id).all()
 
-    return render_template("cliente/historico.html", consultas=consultas)
+    return render_template("cliente/historico.html", consultas=consultas, dietas=dietas)
