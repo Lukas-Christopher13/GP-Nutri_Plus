722a2fe0e54eeeecdcb471d08f6c792df18d6033
Arthur Pereira da Silva <arthurps884@gmail.com>
2024-06-06 16:39:10 -0300

    purga dados alterações

--- app/services/purge_service.py
+++ app/services/purge_service.py
@@ -8,9 +8,9 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 
 
-def purge_old_records(model, date_column, retention_days):
-    cutoff_date = datetime.utcnow() - timedelta(days=retention_days)
-    old_records = db.session.query(model).filter(date_column < cutoff_date).all()
+def purge_dados(model, date_column, retention_days):
+    differences = datetime.utcnow() - timedelta(days=retention_days)
+    old_records = db.session.query(model).filter(date_column < differences).all()
     
     for record in old_records:
         db.session.delete(record)
@@ -19,15 +19,15 @@ def purge_old_records(model, date_column, retention_days):
     
     print(f"{len(old_records)} registros antigos foram purgados da tabela {model.__tablename__}.")
 
-def purge_all_old_records():
+def exec():
     retention_days = 365
-    purge_old_records(Consulta, Consulta.date, retention_days)
-    purge_old_records(Diet, Diet.date, retention_days)
-    purge_old_records(Activity, Activity.date, retention_days)
-    purge_old_records(Exam, Exam.date, retention_days)
+    purge_dados(Consulta, Consulta.date, retention_days)
+    purge_dados(Diet, Diet.date, retention_days)
+    purge_dados(Activity, Activity.date, retention_days)
+    purge_dados(Exam, Exam.date, retention_days)
 
 def init_purge_sheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=purge_all_old_records, trigger="interval", days=1)
+    scheduler.add_job(func=exec, trigger="interval", days=1)
     scheduler.start()
 
